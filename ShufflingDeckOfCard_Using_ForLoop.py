39d99779da7525bdc6539e3022ddb848580492ae
Narenkumar Moorthy <44838834+narenkumarbe@users.noreply.github.com>
2019-04-11 08:18:14 +0200

    Update ShufflingDeckOfCard_Using_ForLoop.py

--- ShufflingDeckOfCard_Using_ForLoop.py
+++ ShufflingDeckOfCard_Using_ForLoop.py
@@ -19,3 +19,57 @@ random.shuffle(deck)
 
 for m in range(52):
     print(deck[m])
+
+Output:
+J of Diamond
+3 of Spade
+6 of Spade
+5 of Club
+10 of Diamond
+6 of Diamond
+J of Club
+5 of Spade
+K of Diamond
+2 of Club
+9 of Diamond
+8 of Diamond
+A of Diamond
+5 of Hearts
+4 of Hearts
+Q of Spade
+A of Club
+J of Hearts
+8 of Hearts
+9 of Club
+A of Hearts
+7 of Spade
+9 of Spade
+4 of Club
+6 of Club
+8 of Spade
+6 of Hearts
+J of Spade
+2 of Diamond
+3 of Club
+9 of Hearts
+5 of Diamond
+7 of Diamond
+4 of Spade
+Q of Hearts
+K of Club
+Q of Diamond
+10 of Spade
+10 of Club
+2 of Hearts
+4 of Diamond
+7 of Club
+7 of Hearts
+3 of Diamond
+Q of Club
+3 of Hearts
+K of Spade
+8 of Club
+K of Hearts
+A of Spade
+10 of Hearts
+2 of Spade
